d078ffc6f9608274aeb425e0551deff749d42754
Martin Leclerc <clercc77@hotmail.com>
2021-10-17 10:59:41 -0400

    Media sociaux

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -10,7 +10,10 @@ class Home extends Component {
             <div className="condiv home">
             <img src={profilepic} alt="ProfilePic" className="profilepic"></img>
             <ReactTypingEffect className="typingeffect" text={['Je suis Martin Dufresne','Je suis développeur web']} speed={100} eraseDelay={700}/>
-
+            <div className="mediaSociaux">
+                <a href="https://fr-ca.facebook.com/" class="fa fa-facebook"></a>
+                <a href="https://twitter.com/?lang=fr" class="fa fa-twitter"></a>
+            </div>
             </div>
             )
         }
